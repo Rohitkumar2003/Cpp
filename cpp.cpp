03c340cad615499080b0b1571f1709e69351a39e
Rohit Kumar <Mr.developer2003@gmail.com>
2024-07-27 20:02:43 +0530

    sorted Array

--- cpp.cpp
+++ cpp.cpp
@@ -2,22 +2,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int firstOccurrence(int arr[], int size, int targeted__Value) {
+int firstOccurrence(int arr[], int size, int targeted__value) {
 	int start = 0; int end = size-1; int mid = start+(end-start)/2; int ans = -1;
-			while(start<=end) {
-				if(arr[mid]==targeted__Value) {
-					ans = mid;
-					start = mid+1;
-				}else if(arr[mid]<targeted__Value) {
-					end = mid-1;
-				}else {
-					start = mid+1;
-				}
-				int mid = start+(end-start)/2;
+		while(start<=end ){
+			if(arr[mid]==targeted__value){
+				ans = mid;
+				start = mid+1;
+			}else if(arr[mid]<targeted__value) {
+				end = mid-1;
+			}else {
+				start = mid+1;
 			}
-			return ansl
+			int mid = start+(end-start)/2;
+		}
+		return ans;
 }
-
 
 int main(int argc, char const *argv[]){
 	
