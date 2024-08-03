f0dbe230b6afe4a9178476e351b2e8dfc15806f4
Rohit Kumar <Mr.developer2003@gmail.com>
2024-08-03 13:56:12 +0530

    Operators

--- cpp.cpp
+++ cpp.cpp
@@ -8,14 +8,19 @@ int main(int argc, char const *argv[]){
 
 			//  Size of datatype
 
-			cout<<sizeof(int)<<endl;
-			cout<<sizeof(char)<<endl;
-			cout<<sizeof(bool)<<endl;
-			cout<<sizeof(float)<<endl;
-			cout<<sizeof(double)<<endl;
-			cout<<sizeof(long long)<<endl;
+			// cout<<sizeof(int)<<endl;
+			// cout<<sizeof(char)<<endl;
+			// cout<<sizeof(bool)<<endl;
+			// cout<<sizeof(float)<<endl;
+			// cout<<sizeof(double)<<endl;
+			// cout<<sizeof(long long)<<endl;
 
-			// size of varible
+			// Arithmetic operation 
+			
+			int x, y;
+			cin>>x>>y;
+
+			cout << " values:  " << x+y << endl << x -y << endl << x/2 << endl;			
 
 		
 	
