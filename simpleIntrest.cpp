789e78aeefc6f0040da08ef2a21f4ad5f323afeb
Rohit Kumar <Mr.developer2003@gmail.com>
2024-08-03 14:05:32 +0530

    simple intrest

---
+++ simpleIntrest.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    
+  
+    int p,t,r;
+    cin>>p>>t>>r;
+
+    cout<<(p*r*t/100) << endl;
+
+    return 0;
+}
