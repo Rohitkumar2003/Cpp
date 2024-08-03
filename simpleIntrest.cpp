c246ad7aaaed306cd8dfc9b764dffe6fb0d5e692
Rohit Kumar <Mr.developer2003@gmail.com>
2024-08-03 14:25:03 +0530

    new

--- simpleIntrest.cpp
+++ simpleIntrest.cpp
@@ -4,10 +4,18 @@ using namespace std;
 int main(int argc, char const *argv[]){
     
   
-    int p,t,r;
-    cin>>p>>t>>r;
+  
+    // int p,t,r;
+    // cin>>p>>t>>r;
 
-    cout<<(p*r*t/100) << endl;
+    // cout<<(p*r*t/100) << endl;
+
+    int x=7,y=-3;
+
+    cout<<(x & y)<< endl;
+
+
+
 
     return 0;
 }
